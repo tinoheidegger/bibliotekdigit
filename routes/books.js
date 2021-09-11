53095feac3e4a6877199c0600f98061a7b92af8a
tinoheidegger <tinoheidegger83@gmail.com>
2021-09-11 13:03:02 +0000

    Routes de Livres Terminées

--- routes/books.js
+++ routes/books.js
@@ -54,15 +54,84 @@ router.post("/", async (req, res) => {
   }
 });
 
+//show book route
+router.get("/:id", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id).populate("author").exec();
+    res.render("books/show", { book: book });
+  } catch {}
+});
+
+//edit book route
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const book = Book.findById(req.params.id);
+    renderEditPage(res, book);
+  } catch {
+    res.redirect("/");
+  }
+});
+
+// Update Book Route
+router.put("/:id", async (req, res) => {
+  let book;
+  try {
+    book = await Book.findById(req.params.id);
+    book.title = req.body.title;
+    book.author = req.body.author;
+    book.publishDate = new Date(req.body.publishDate);
+    book.pageCount = req.body.pageCount;
+    book.description = req.body.description;
+    if (req.body.cover != null && req.body.cover !== "") {
+      saveCover(book, req.body.cover);
+    }
+    await book.save();
+    res.redirect(`/books/${book.id}`);
+  } catch {
+    if (book != null) {
+      renderEditPage(res, book, true);
+    } else {
+      redirect("/");
+    }
+  }
+});
+
+// Delete Book Page
+router.delete("/:id", async (req, res) => {
+  let book;
+  try {
+    book = await Book.findById(req.params.id);
+    await book.remove();
+    res.redirect("/books");
+  } catch {
+    if (book != null) {
+      res.render("books/show", {
+        book: book,
+        errorMessage: "Impossible de supprimer le livre",
+      });
+    } else {
+      res.redirect("/");
+    }
+  }
+});
+
 async function renderNewPage(res, book, hasError = false) {
+  renderFormPage(res, book, "new", hasError);
+}
+
+async function renderEditPage(res, book, hasError = false) {
+  renderFormPage(res, book, "edit", hasError);
+}
+
+async function renderFormPage(res, book, form, hasError = false) {
   try {
     const authors = await Author.find({});
     const params = {
       authors: authors,
       book: book,
     };
-    if (hasError) params.errorMessage = "Erreur dans la création du livre";
-    res.render("books/new", params);
+    if (hasError) params.errorMessage = "Création de livre échouée";
+    res.render(`books/${form}`, params);
   } catch {
     res.redirect("/books");
   }
